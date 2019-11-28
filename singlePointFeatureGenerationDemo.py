2905a8492a323f79a0c5d98aa9cb24ac2ac27a49
societynong <172065208@qq.com>
2019-11-28 17:47:18 +0800

    1128

--- singlePointFeatureGenerationDemo.py
+++ singlePointFeatureGenerationDemo.py
@@ -3,4 +3,5 @@ from featureUtils import *
 if __name__ == "__main__":
     snr = -45
     featureName = 'SinglePoint'
-    dataGeneration(featureName,-45,50)
+    nPoint = 50
+    dataGeneration(featureName,snr,getSiglePointFeature,(FS,F0),nPoint)
